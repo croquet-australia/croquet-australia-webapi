c765b889be88d3fb41e0f777961a142db585a132
Tim Murphy <tim@26tp.com>
2016-01-27 18:02:47 +1100

    Add solution and projects

---
+++ source/CroquetAustralia.WebApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("CroquetAustralia.WebApi")]
+[assembly: AssemblyDescription("Croquet Australia's Web API - https://api.croquet-australia.com.au/")]
---
+++ source/CroquetAustralia.WebApi/Properties/CommonAssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Croquet Australia")]
+[assembly: AssemblyProduct("Croquet Australia")]
+[assembly: AssemblyCopyright("Copyright © 2016 Tim Murphy")]
+[assembly: ComVisible(false)]
+
+// todo: Version number updated by build system
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
